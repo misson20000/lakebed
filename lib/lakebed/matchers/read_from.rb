6a92acc45d7cb0243ac32deda37c47a9c86cd0af
misson20000 <xenotoad@xenotoad.net>
2018-11-11 17:25:00 -0800

    read_from fix

--- lib/lakebed/matchers/read_from.rb
+++ lib/lakebed/matchers/read_from.rb
@@ -5,44 +5,44 @@ module Lakebed
     RSpec::Matchers.define :read_from do |a, b, c=nil|
       match do |subject|
         if subject.is_a? Proc then
-          emu = a
-          address = b
-          match_size = c
+          @emu = a
+          @address = b
+          @match_size = c
         else
-          emu = subject
-          address = a
-          match_size = b
+          @emu = subject
+          @address = a
+          @match_size = b
         end
         
-        if match_size == nil then
-          match_size = 8
+        if @match_size == nil then
+          @match_size = 8
         end
 
         @read_size = nil
         proc = Proc.new do |uc, access, address, size, value|
           @read_size = size
-          emu.mu.emu_stop
+          @emu.mu.emu_stop
         end # keep this in a local variable since unicorn gem doesn't do GC right...
 
-        hook = emu.mu.hook_add(UnicornEngine::UC_HOOK_MEM_READ, proc, nil, address, address + match_size - 1)
+        hook = @emu.mu.hook_add(UnicornEngine::UC_HOOK_MEM_READ, proc, nil, @address, @address + @match_size - 1)
 
         if subject.is_a? Proc then
           subject.call
         else
-          emu.begin
+          @emu.begin
         end
         
-        emu.mu.hook_del(hook)
+        @emu.mu.hook_del(hook)
 
-        @pc = emu.pc
+        @pc = @emu.pc
         
-        return @read_size == match_size
+        return @read_size == @match_size
       end
 
       supports_block_expectations
       
       failure_message do |actual|
-        first_line = "expected that emulator would read 0x#{match_size.to_s(16)} bytes from 0x#{address.to_s(16)}"
+        first_line = "expected that emulator would read 0x#{@match_size.to_s(16)} bytes from 0x#{@address.to_s(16)}"
         if @read_size then
           second_line = "read: #{@read_value.unpack("H*").first}"
         else
