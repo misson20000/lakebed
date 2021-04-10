4d70d259de6af7124434144486df16cb203a4657
misson20000 <xenotoad@xenotoad.net>
2021-04-10 15:36:42 -0700

    let log_for_thread take some extra parameters

--- lib/lakebed/logger.rb
+++ lib/lakebed/logger.rb
@@ -12,9 +12,17 @@ module Lakebed
       puts "[" + Paint[b.to_s.ljust(THREAD_COL_WIDTH), :yellow, :bright] + "] " + Paint["CONTEXT SWITCH", :yellow]
     end
     
-    def self.log_for_thread(thread, msg)
+    def self.log_for_thread(thread, msg, params={})
       indent = " " * (THREAD_COL_WIDTH + 3)
       msg = msg.lines.join(indent)
+
+      if params.length != 0 then
+        msg+= ": "
+        msg+= params.map do |k, v|
+          ":" + k.to_s + " => " + v.to_s
+        end.join(", ")
+      end
+      
       if @thread != thread then
         puts Paint["[#{thread.to_s.ljust(THREAD_COL_WIDTH)}] ", :gray] + msg
       else
