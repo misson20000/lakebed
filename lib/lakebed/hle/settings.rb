ebd92dfd28e08969268b4a7b4aceae81b46b4a50
misson20000 <xenotoad@xenotoad.net>
2023-05-28 13:59:16 -0400

    add settings hle

---
+++ lib/lakebed/hle/settings.rb
@@ -0,0 +1,20 @@
+require_relative "hle_service.rb"
+
+module Lakebed
+  module HLE
+    class SettingsServer < HLEService
+      def register_services
+        register("set:cal") do
+          IFactorySettingsServer.new
+        end
+      end
+      
+      class IFactorySettingsServer < CMIF::Object
+        command(
+          16, :GetSslKey,
+          CMIF::Buffer.new(0x16, "key", 0x130)) do |output|
+        end
+      end
+    end
+  end
+end
